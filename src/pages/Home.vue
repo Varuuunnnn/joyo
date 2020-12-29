311a0cccd49418a7fa85d15583e22ceef64d2249
Varun <varungurjar@cilalabs.com>
2020-12-29 18:36:41 +0530

    Fix width

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -48,8 +48,9 @@ export default {
 <style lang="stylus" scoped>
 .full-width {
     width: 60% !important;
-    margin-left: 250px !important /* rtl:ignore */;
+    margin-left: 290px !important /* rtl:ignore */;
     margin-right: 250px !important /* rtl:ignore */;
+    margin-top: 100px !important /* rtl:ignore */;
     margin-bottom 400px !important /* rtl:ignore */;
 }
 </style>

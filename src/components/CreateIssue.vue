f30ee92937231961ec42a508bd8377ae5076b4dc
varun <varun.gurjar@cilalabs.com>
2021-01-01 23:31:18 +0530

    Add components

---
+++ src/components/CreateIssue.vue
@@ -0,0 +1,50 @@
+<template>
+    <q-form v-if="tab=='create-issue'" @submit="createIssue">
+       <q-input
+         outlined
+         class="q-mb-md"
+         v-model="projectData.title"
+         label="Project title"
+         type="text"
+      />
+      <q-input
+         outlined
+         class="q-mb-md"
+         v-model="projectData.issues"
+         label="Project issues"
+         type="textarea"
+      />
+     
+      <div style="width:130px; margin:0 auto;">
+          <q-space />
+          <q-btn
+             color="primary"
+             label="Create issue" 
+             type="submit"
+           />
+      </div>
+    </q-form>    
+</template>
+<script>
+
+export default {
+    props:['tab'],
+    data(){
+        return{
+            projectIssues:[],
+            projectData:{
+                title:'',
+                issue:'',
+                // we can use issueId 
+            }
+        }
+    },
+    methods:{
+        createIssue(){
+         this.projectIssues.push(this.projectData)
+         this.projectData={}
+        }
+    }
+    
+}
+</script>
--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -1,74 +1,135 @@
 <template>
-  <div class="q-pa-md" >
-      
-      <div class="text-h2 text-center">
-         {{ userDetails.name }}
-         <q-badge>{{ userDetails.type }}</q-badge>
-      </div>
-      
-      
-      <q-card class="full-width">
-        <q-tabs
-          v-model="tab"
-          class="q-mb-md"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="add-project" label="Add project" v-if="userDetails.type=='admin'" />
-          <q-tab name="project-list" label="Projects list" />
-          
-        </q-tabs>
-
-        <q-separator />
-
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="add-project" v-if="userDetails.type=='admin'">
-            <add-project :tab="tab" />
-            
-          </q-tab-panel>
-
-          <q-tab-panel name="project-list">
-            <project-list :tab="tab" />
-           
-          </q-tab-panel>
-
-          
-        </q-tab-panels>
-      </q-card>
-
-  
+  <div class="q-pa-md">
+    <div class="text-h2 text-center">
+      {{ userDetails.name }}
+      <q-badge>{{ userDetails.type }}</q-badge>
     </div>
-  
+    <div class="q-mt-md">
+      <q-fab
+        v-model="fab2"
+        label="What to do"
+        external-label
+        label-class="bg-grey-3 text-purple"
+        vertical-actions-align="left"
+        color="primary"
+        icon="menu"
+        direction="down"
+      >
+        <q-fab-action
+          label-class="bg-grey-3 text-grey-8"
+          external-label
+          color="primary"
+          @click="onClick"
+          icon="add_task"
+          label="Add project"
+        />
+        <q-fab-action
+          label-class="bg-grey-3 text-grey-8"
+          external-label
+          color="secondary"
+          @click="onClick"
+          icon="list"
+          label="Projects list"
+        />
+        <q-fab-action
+          label-class="bg-grey-3 text-grey-8"
+          external-label
+          color="red"
+          @click="onClick"
+          icon="warning"
+          label="Create issues"
+        />
+        <q-fab-action
+          label-class="bg-grey-3 text-grey-8"
+          external-label
+          color="accent"
+          @click="onClick"
+          icon="comment"
+          label="Add comments"
+        />
+      </q-fab>
+    </div>
+
+    <q-card class="full-width">
+      <q-tabs
+        v-model="tab"
+        class="q-mb-md"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab
+          name="add-project"
+          label="Add project"
+          v-if="userDetails.type == 'admin'"
+        />
+        <q-tab 
+          name="project-list"
+          label="Projects list" 
+        />
+        <q-tab 
+          v-if="userDetails.type == 'admin'||userDetails.type == 'projectManager'||userDetails.type == 'projectLead'"
+          name="create-issue"
+          label="Create issue" 
+        />
+        <q-tab 
+          name="add-comment"
+          label="Add comment" 
+        />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="add-project" v-if="userDetails.type == 'admin'">
+          <add-project :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="project-list">
+          <project-list :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="create-issue" v-if="userDetails.type == 'admin'||userDetails.type == 'projectManager'||userDetails.type == 'projectLead'">
+          <create-issue :tab="tab" />
+        </q-tab-panel>
+
+        <q-tab-panel name="add-comment" >
+          <add-comment :tab="tab" />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </div>
 </template>
 
 <script>
 
-import { mapState} from 'vuex'
+import { mapState } from "vuex";
 export default {
-  computed:{
-    ...mapState('store',['userDetails'])
+  computed: {
+    ...mapState("store", ["userDetails"]),
   },
-  data () {
+  data() {
     return {
-      tab: 'project-list'
-    }
+      tab: "project-list",
+      fab2: true,
+    };
   },
-  components:{
-    'add-project':require('components/AddProjects.vue').default,
-    'project-list':require('components/ShowProjectsList.vue').default
-    
+  components: {
+    "add-project": require("components/AddProjects.vue").default,
+    "project-list": require("components/ShowProjectsList.vue").default,
+    "create-issue": require("components/CreateIssue.vue").default,
+    "add-comment": require("components/AddComment.vue").default
   }
-
-}
+};
 </script>
+    AddComment
 <style lang="stylus" scoped>
 .full-width {
-    width: 60% !important;
-    margin-left: 290px !important /* rtl:ignore */;
-    margin-right: 250px !important /* rtl:ignore */;
-    margin-top: 50px !important /* rtl:ignore */;
-    margin-bottom 400px !important /* rtl:ignore */;
+  width: 60% !important;
+  margin-left: auto !important; /* rtl:ignore */
+  margin-right: auto !important; /* rtl:ignore */
+  margin-top: auto !important; /* rtl:ignore */
+  margin-bottom: auto !important; /* rtl:ignore */
 }
 </style>
